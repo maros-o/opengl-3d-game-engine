55939aeb943a342ec80a6c211850eb3c72a37313
maros-o <maros.mec@seznam.cz>
2023-10-09 00:33:34 +0200

    composite transformations

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -30,6 +30,7 @@ void Renderer::update_model_matrices() {
     }
 }
 
+//
 void Renderer::render_objects(OrthoCamera *camera) {
     for (auto &model_group: this->objects) {
         auto model = this->models[model_group.first];
